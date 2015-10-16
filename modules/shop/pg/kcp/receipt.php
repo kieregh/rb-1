68db8e974c2d8707ea3adc98dd3236b245f8eecf
gitaeks <break@kimsq.com>
2015-10-16 20:52:19 +0900

    저장소 초기화

--- modules/shop/pg/kcp/receipt.php
+++
@@ -1,7 +0,0 @@
-
-<script type="text/javascript">
-//<![CDATA[
-top.resizeTo(450,650);
-location.href = 'http://admin.kcp.co.kr/Modules/Sale/Card/ADSA_CARD_BILL_Receipt.jsp?c_trade_no=<?php echo $O['tid']?>';
-//]]>
-</script>
